1af042024b417ae93b122bbe241f867e00bafec6
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-03-03 06:50:28 +0300

    Improved 'react-dom/client' ERROR

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import CitySearch from './CitySearch';
 import EventList from './EventList';
 import NumberOfEvents from './NumberOfEvents';
+import { mockData } from './mock-data';
 
 function App() {
   return (
     <div className="App">
-      <EventList />
+      <EventList events={mockData}/>
       <CitySearch />
       <NumberOfEvents />
     </div>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom/client'; <- This import is only for React version 18
+import { render } from 'react-dom'; // <- This is the correct import statement for React version 17
 import './index.css';
-import { App } from './App';
+import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = document.getElementById('root'); // <- This is the correct method call for React version 17
+render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  root
 );
 
 // If you want your app to work offline and load faster, you can change
