fabf461f0698faf5217825408b2011b2226669c5
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-03-03 04:31:09 +0300

    Added Event component for single event

---
+++ src/Event.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+
+class Event extends Component {
+
+  state = {
+    details: false
+  };
+
+  toggleDetails = () => {
+    this.setState((prevState) => ({
+      details: !prevState.details
+    }))
+  }
+
+  render() {
+
+    const { event } = this.props;
+    const { details } = this.state;
+
+    return (
+        <div className="event">
+            <h1 className="event-summary">
+              {event.summary}
+            </h1>
+            <p className="event-start">
+              {new Date(event.start.dateTime).toString()}
+            </p>
+            <p className="event-location">
+              {`@${event.summary} | ${event.location}`}
+            </p>
+
+            {
+              details && (
+                <div className="event-details">
+                  <h2 className="about-event-header">
+                    About event:
+                  </h2>
+                  <a className="event-link" href={event.htmlLink} target="_blank" rel="noopener noreferrer">
+                    See details on Google Calendar
+                  </a>
+                  <p className="event-description">
+                    {event.description}
+                  </p>
+                </div>
+              )
+            }
+
+            <button className="details-button" onClick={() => this.toggleDetails()}>
+              {details ? "hide details" : "show details"}
+            </button>
+        </div>
+
+    );
+
+  }
+}
+export default Event;
