9726c8b766f4f615ee93f127966fc20b3b2b854f
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-03-08 01:15:47 +0300

    Added event updating and event state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import './nprogress.css'
 import CitySearch from './CitySearch';
 import EventList from './EventList';
 import NumberOfEvents from './NumberOfEvents';
 import { mockData } from './mock-data';
+import { extractLocations, getEvents } from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <EventList events={mockData}/>
-      <CitySearch />
-      <NumberOfEvents />
-    </div>
-  );
+class App extends Component {
+  
+  state = {
+    events: [],
+    locations: []
+  };
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = (location === 'all') ? 
+      events :
+      events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <CitySearch locations = {this.state.locations} updateEvents={this.updateEvents}/>
+        <NumberOfEvents />
+        <EventList events = {this.state.events} />
+      </div>
+    );
+  }
 }
 
 export default App;
