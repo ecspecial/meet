fddddff2a1c144e08d6f0cc2a0997ceff4e8ad90
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-03-09 00:50:32 +0300

    Implemented displaying of eventlist according to eventsNumber

--- src/EventList.js
+++ src/EventList.js
@@ -3,10 +3,10 @@ import Event from "./Event";
 
 class EventList extends Component {
     render() {
-        const { events } = this.props;
+        const { events, eventsNumber } = this.props;
         return (
             <ul className="EventList">
-                {events.map(event =>
+                { (eventsNumber ? events.slice(0, eventsNumber) : events).map(event =>
                     <li key={event.id}>
                         <Event event={event} />
                     </li>
