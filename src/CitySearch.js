176e8d7df30899795aebcb82d4f6962ccaf0f1ee
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-03-08 04:31:35 +0300

    Set CitySearch to show all cities by default

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -45,6 +45,12 @@ class CitySearch extends Component {
         this.props.updateEvents(suggestion);
     };
 
+    handleFocus = () => {
+        this.setState({
+            showSuggestions: true,
+        });
+    };
+
     render() {
         return (
             <div className="CitySearch" ref={(ref) => (this.searchContainer = ref)}>
@@ -54,20 +60,31 @@ class CitySearch extends Component {
                     className = "city"
                     value = {this.state.query}
                     onChange = {this.handleInputChanged}
-                    onFocus = { () => { this.setState({ showSuggestions: true }) }}
+                    onFocus = {this.handleFocus}
                 />
                 <ul 
                     className="suggestions" 
                     style={this.state.showSuggestions ? {} : { display: 'none' }}
                 >
-                    {this.state.suggestions.map((suggestion) => (
-                        <li 
-                            key={suggestion}
-                            onClick={() => this.handleItemClicked(suggestion)}
-                        >
-                            {suggestion}
-                        </li>
-                    ))}
+                    {
+                        this.state.query === "" ?
+                        this.props.locations.map((suggestion) => (
+                            <li 
+                                key={suggestion}
+                                onClick={() => this.handleItemClicked(suggestion)}
+                            >
+                                {suggestion}
+                            </li>
+                        )) :
+                        this.state.suggestions.map((suggestion) => (
+                            <li 
+                                key={suggestion}
+                                onClick={() => this.handleItemClicked(suggestion)}
+                            >
+                                {suggestion}
+                            </li>
+                        ))
+                    }
                     <li key="all" onClick={() => this.handleItemClicked("all")}>
                         <b>See all cities</b>
                     </li>
