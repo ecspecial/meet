d619136880582c5db50ee364197cbf125b7ba9e9
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-03-08 03:44:18 +0300

    Added hiding search box if clicked outside

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
 
 class CitySearch extends Component {
-    
     state = {
         query: '',
         suggestions: [],
-        showSuggestions: undefined
-      }
+        showSuggestions: false // initialize showSuggestions to false
+    };
+
+    componentDidMount() {
+        // add a click event listener to the window object
+        window.addEventListener("click", this.handleClickOutside);
+    }
+
+    componentWillUnmount() {
+        // remove the click event listener when the component unmounts
+        window.removeEventListener("click", this.handleClickOutside);
+    }
+
+    handleClickOutside = (event) => {
+        // if the user clicked outside of the CitySearch component, hide the suggestions
+        if (this.searchContainer && !this.searchContainer.contains(event.target)) {
+            this.setState({ showSuggestions: false });
+        }
+    };
 
     handleInputChanged = (event) => {
         const value = event.target.value;
@@ -16,13 +32,14 @@ class CitySearch extends Component {
         this.setState({
             query: value,
             suggestions,
+            showSuggestions: true // show suggestions when the input is changed
         });
     };
-    
+
     handleItemClicked = (suggestion) => {
         this.setState({
-          query: suggestion,
-          showSuggestions: false
+            query: suggestion,
+            showSuggestions: false // hide suggestions when an item is clicked
         });
 
         this.props.updateEvents(suggestion);
@@ -30,22 +47,26 @@ class CitySearch extends Component {
 
     render() {
         return (
-            <div className="CitySearch">
+            <div className="CitySearch" ref={(ref) => (this.searchContainer = ref)}>
                 <input 
                     type = "text"
+                    placeholder="City"
                     className = "city"
                     value = {this.state.query}
                     onChange = {this.handleInputChanged}
                     onFocus = { () => { this.setState({ showSuggestions: true }) }}
                 />
-                <ul className="suggestions" style={ 
-                    this.state.showSuggestions? {}: { display: 'none' }
-                    }>
+                <ul 
+                    className="suggestions" 
+                    style={this.state.showSuggestions ? {} : { display: 'none' }}
+                >
                     {this.state.suggestions.map((suggestion) => (
                         <li 
-                        key={suggestion}
-                        onClick={() => this.handleItemClicked(suggestion)}
-                        >{suggestion}</li>
+                            key={suggestion}
+                            onClick={() => this.handleItemClicked(suggestion)}
+                        >
+                            {suggestion}
+                        </li>
                     ))}
                     <li key="all" onClick={() => this.handleItemClicked("all")}>
                         <b>See all cities</b>
