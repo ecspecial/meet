29187100e10708740ceadc68b2deff62c87e87c0
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-03-20 03:47:36 +0300

    Implemented Atatus for app performance monitoring

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import * as atatus from 'atatus-spa';
 // import ReactDOM from 'react-dom/client'; <- This import is only for React version 18
 import { render } from 'react-dom'; // <- This is the correct import statement for React version 17
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+
+atatus.config('7f44376d0db244ef8713c95fb3d7af9a').install();
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 const root = document.getElementById('root'); // <- This is the correct method call for React version 17
