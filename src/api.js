672d4b13b7ef5b4b0a6b3b2c138e4e182fc051f0
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-03-22 06:22:37 +0300

    Repositioned function to deal with loading order

--- src/api.js
+++ src/api.js
@@ -33,12 +33,6 @@ export const getAccessToken = async () => {
     return accessToken;
 };
 
-export const extractLocations = (events) => {
-    var extractLocations = events.map((event) => event.location);
-    var locations = [...new Set(extractLocations)];
-    return locations;
-};
-
 export const getEvents = async () => {
     NProgress.start();
     
@@ -72,6 +66,12 @@ export const getEvents = async () => {
 
         }
     }
+};
+
+export const extractLocations = (events) => {
+    var extractLocations = events.map((event) => event.location);
+    var locations = [...new Set(extractLocations)];
+    return locations;
 };
 
 const removeQuery = () => {
