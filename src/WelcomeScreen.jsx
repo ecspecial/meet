0e01fb540a6c5c4e4548dea4be939e3b8670c294
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-03-26 23:20:05 +0300

    Impoved button styling

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -9,26 +9,26 @@ function WelcomeScreen(props) {
             <h4>
             Log in to see upcoming events around the world for full-stack developers
             </h4>
-            <div className="button_cont" align="center">
-            <div className="google-btn flex items-center space-x-2">
-                <div className="google-icon-wrapper">
-                  <img
-                    className="google-icon"
-                    src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-                    alt="Google sign-in"
-                   />
+            <div className="button_cont flex items-center space-x-2" align="center">
+                <div className="google-btn">
+                    <div className="google-icon-wrapper">
+                      <img
+                        className="google-icon"
+                        src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                        alt="Google sign-in"
+                       />
+                    </div>
+                        <button
+                            className="btn-text px-3 py-2 rounded-md text-white font-bold bg-blue-500 hover:bg-blue-700"
+                            onClick={() => {
+                            props.getAccessToken();
+                            }}
+                            rel="nofollow noopener"
+                        >
+                            Sign in with Google
+                        </button>
+                    </div>
                 </div>
-                 <button
-                  className="btn-text px-3 py-2 rounded-md text-white font-bold bg-blue-500 hover:bg-blue-700"
-                   onClick={() => {
-                     props.getAccessToken();
-                   }}
-                   rel="nofollow noopener"
-                 >
-                   Sign in with Google
-                 </button>
-                </div>
-            </div>
             <a
                 href="https://ecspecial.github.io/meet/privacy.html"
                 rel="nofollow noopener"
