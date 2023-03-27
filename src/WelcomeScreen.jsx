e93babb01f74425c75b6584f8530f1bf7f973d27
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-03-27 04:58:43 +0300

    Improved styling

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -5,35 +5,35 @@ function WelcomeScreen(props) {
     return props.showWelcomeScreen ?
     (
         <div className="WelcomeScreen">
-            <h1>Welcome to the Meet app</h1>
-            <h4>
+        <h1>Welcome to the Meet app</h1>
+        <h4>
             Log in to see upcoming events around the world for full-stack developers
-            </h4>
-            <div className="button_cont">
-                <button
-                    className="google-btn"
-                    onClick={() => {
-                        props.getAccessToken();
-                    }}
-                    rel="nofollow noopener"
-                >
-                    <div className="google-icon-wrapper">
-                        <img
-                            className="google-icon"
-                            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-                            alt="Google sign-in"
-                        />
-                    </div>
-                    <span className="btn-text">Sign in with Google</span>
-                </button>
+        </h4>
+        <div className="google-btn">
+            <div className="google-icon-wrapper">
+                <img
+                    className="google-icon"
+                    src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                    alt="Google sign-in"
+                />
             </div>
-            <a
-                href="https://ecspecial.github.io/meet/privacy.html"
+            <button
+                onClick={() => {
+                props.getAccessToken();
+                }}
                 rel="nofollow noopener"
+                className="btn-text"
             >
-                Privacy policy
-            </a>
+                <b>Sign in with google</b>
+            </button>
         </div>
+        <a
+            href="https://ecspecial.github.io/meet/privacy.html"
+            rel="nofollow noopener"
+        >
+            Privacy policy
+        </a>
+    </div>
     )
 : null
 }
