55f170592f6f8af7f6b22baf0619bfade85b8ff7
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-03-29 01:13:31 +0300

    Added function to return City and Number of events in city

--- src/App.js
+++ src/App.js
@@ -34,6 +34,16 @@ class App extends Component {
     this.setState({ eventsNumber });
   };
 
+  getData = () => {
+    const { events, locations } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location).length;
+      const city = location.split(', ').shift();
+      return { city, number };
+    })
+    return data;
+  };
+
   async componentDidMount() {
     this.mounted = true;
     const accessToken = localStorage.getItem('access_token');
